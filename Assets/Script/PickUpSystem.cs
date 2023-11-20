1430f5451a77110b3e0d238d8c1eab516f952f2b
h3lpmeimdead <vietuc.2004@gmail.com>
2023-11-21 03:30:13 +0700

    f

--- Assets/Script/PickUpSystem.cs
+++ Assets/Script/PickUpSystem.cs
@@ -15,30 +15,55 @@ public class PickUpSystem : MonoBehaviour
         uiScript = GetComponent<UIScript>();   
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) //when player enter the collision box
+    //private void OnTriggerEnter2D(Collider2D collision) //when player enter the collision box
+    //{
+    //    if(collision.CompareTag("Interactable")) //if the item has the tag "Interactable"
+    //    {
+    //        //uiScript.ShowText("E to pick up");
+    //        if(Input.GetKeyDown(KeyCode.E))
+    //        {
+    //            PickUpItem(collision.gameObject);
+    //        }
+    //    }
+    //}
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.CompareTag("Interactable")) //if the item has the tag "Interactable"
-        {
-            uiScript.ShowText("E to pick up");
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-
-            }
-        }
+        
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(holdItem == null && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUpItem();
+        }
+        if(holdItem != null && Input.GetKeyDown(KeyCode.Q))
+        {
+            DropItem();
+        }
     }
 
     public void PickUpItem()
     {
-
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.5f);
+        foreach(Collider2D collider in colliders)
+        {
+            if(collider.CompareTag("Interactable"))
+            {
+                holdItem = collider.gameObject;
+                holdItem.transform.SetParent(transform); // set the object to be the child of the player
+                holdItem.transform.localPosition = Vector3.zero;
+                holdItem.GetComponent<Collider2D>().enabled = false; // set the collider of the object to false
+                break;
+            }
+        }
+        
     }
 
     public void DropItem()
     {
-
+        holdItem.GetComponent<Collider2D>().enabled = true;
+        holdItem.transform.SetParent(null);
+        holdItem = null;
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,18 +7,12 @@ public class Player : MonoBehaviour
     public Joystick movementJoystick;
     public float playerSpeed;
     private Rigidbody2D rb;
-    //[SerializeField] CameraShakeScript cameraShake;
-    //private float intensity = 1f;
-    // Start is called before the first frame update
+   
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //cameraShake = GetComponent<CameraShakeScript>();
     }
-    //private void ShakingCam()
-    //{
-    //    cameraShake.ShakeCamera(intensity);
-    //}
+
     // Update is called once per frame
     void FixedUpdate()
     {
